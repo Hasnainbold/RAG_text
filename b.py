db5826449eb2701515aa159055beb2dba6ea5496
Hasnain shaikh <114347774+Hasnainbold@users.noreply.github.com>
2024-09-19 16:16:37 +0530

    Update b.py

--- b.py
+++ b.py
@@ -86,7 +86,7 @@ from dspy.evaluate import Evaluate
 from dspy.retrieve.weaviate_rm import WeaviateRM
 from dspy.retrieve.pinecone_rm import PineconeRM
 
-
+os.environ["url"] =st.secrets["url"]
 client = weaviate.connect_to_wcs(
     cluster_url=url,
     auth_credentials=weaviate.classes.init.Auth.api_key(WEAVIATE_API_KEY),
